e19a89bc7b24c4bedf3880278086f13506a68745
Timóteo <mechamutoh@gmail.com>
2015-11-08 16:20:29 -0400

    Adicionado em Usuario os dados da tabela no banco

--- app/src/main/java/com/infact/nightour/model/Usuario.java
+++ app/src/main/java/com/infact/nightour/model/Usuario.java
@@ -4,4 +4,9 @@ package com.infact.nightour.model;
  * Created by Timóteo on 08/11/2015.
  */
 public class Usuario {
+    public static String BD_TABELA = "usuarios";
+
+    public static String BD_ID = "_id";
+    public static String BD_EMAIL = "email";
+    public static String BD_SENHA = "senha";
 }
