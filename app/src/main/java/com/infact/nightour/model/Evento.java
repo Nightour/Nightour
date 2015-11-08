29acc4899770b6ad25dd8f3dbda0ec5f3995fbaa
Timóteo <mechamutoh@gmail.com>
2015-11-08 15:52:45 -0400

    Adicionado em Evento os dados da tabela no banco

--- app/src/main/java/com/infact/nightour/model/Evento.java
+++ app/src/main/java/com/infact/nightour/model/Evento.java
@@ -4,4 +4,9 @@ package com.infact.nightour.model;
  * Created by Timóteo on 08/11/2015.
  */
 public class Evento {
+    public static String BD_TABELA = "eventos";
+
+    public static String BD_ID = "_id";
+    public static String BD_NOME = "evento_nome";
+    public static String BD_DESCRICAO = "evento_descricao";
 }
