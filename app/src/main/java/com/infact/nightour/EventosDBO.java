5bcc4e7548f75152733e8511688a66b5597a449b
Timóteo <mechamutoh@gmail.com>
2015-11-08 15:43:44 -0400

    Renomeado EventosDBO para controller.EventosController
    Agora há uma package para os controllers, com.infact.nightour.controller

--- app/src/main/java/com/infact/nightour/EventosDBO.java
+++
@@ -1,7 +0,0 @@
-package com.infact.nightour;
-
-/**
- * Created by Timóteo on 08/11/2015.
- */
-public class EventosDBO {
-}
---
+++ app/src/main/java/com/infact/nightour/controller/EventosController.java
@@ -0,0 +1,13 @@
+package com.infact.nightour.controller;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.infact.nightour.BancoDeDados;
+
+/**
+ * Created by Timóteo on 08/11/2015.
+ */
+public class EventosController {
+    
+}
