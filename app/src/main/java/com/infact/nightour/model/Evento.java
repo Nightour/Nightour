1b8018b027aa92e4f293fecc35e350a5cc7f466a
Timóteo <mechamutoh@gmail.com>
2015-11-08 15:58:19 -0400

    Adicionados os campos privados à classe Evento

--- app/src/main/java/com/infact/nightour/model/Evento.java
+++ app/src/main/java/com/infact/nightour/model/Evento.java
@@ -9,4 +9,32 @@ public class Evento {
     public static String BD_ID = "_id";
     public static String BD_NOME = "evento_nome";
     public static String BD_DESCRICAO = "evento_descricao";
+
+    private String id;
+    private String nome;
+    private String descricao;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
 }
