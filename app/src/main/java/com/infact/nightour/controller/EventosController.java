ebe7338d9550c0b7b6d205418340e1ac08d40337
Timóteo <mechamutoh@gmail.com>
2015-11-08 18:42:18 -0400

    Implementado o método insereEvento em EventosController

--- app/src/main/java/com/infact/nightour/controller/EventosController.java
+++ app/src/main/java/com/infact/nightour/controller/EventosController.java
@@ -1,9 +1,11 @@
 package com.infact.nightour.controller;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 
 import com.infact.nightour.BancoDeDados;
+import com.infact.nightour.model.Evento;
 
 /**
  * Created by Timóteo on 08/11/2015.
@@ -14,5 +16,19 @@ public class EventosController {
 
     public EventosController(Context context) {
         banco = new BancoDeDados(context);
+    }
+
+    public long insereEvento(Evento evento) {
+        ContentValues valores = new ContentValues();
+        valores.put(Evento.BD_NOME, evento.getNome());
+        valores.put(Evento.BD_DESCRICAO, evento.getDescricao());
+        valores.put(Evento.BD_GENERO, evento.getGenero());
+        valores.put(Evento.BD_IMAGEM, evento.getImagemBytes());
+
+        db = banco.getWritableDatabase();
+        long resultado = db.insert(Evento.BD_TABELA, null, valores);
+        db.close();
+
+        return resultado;
     }
 }
