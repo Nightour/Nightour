280666a60bd41eb9d53a10bb3bc5a970bd8bc21f
Timóteo <mechamutoh@gmail.com>
2015-11-22 02:21:13 -0400

    Implementado o construtor de FotosController e seus primeiros campos

--- app/src/main/java/com/infact/nightour/controller/FotosController.java
+++ app/src/main/java/com/infact/nightour/controller/FotosController.java
@@ -1,7 +1,18 @@
 package com.infact.nightour.controller;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.infact.nightour.BancoDeDados;
+
 /**
  * Created by Timóteo on 22/11/2015.
  */
 public class FotosController {
+    private SQLiteDatabase db;
+    private BancoDeDados banco;
+
+    public FotosController(Context context) {
+        banco = new BancoDeDados(context);
+    }
 }
