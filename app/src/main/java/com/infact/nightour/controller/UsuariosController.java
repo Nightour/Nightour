e2e4be4cd6e7f67166491e834b537a8087eaa5a8
Timóteo <mechamutoh@gmail.com>
2015-11-22 02:40:57 -0400

    Implementado UsuariosController.makeContentValues
    Método auxiliar para a criação de ContentValues.

--- app/src/main/java/com/infact/nightour/controller/UsuariosController.java
+++ app/src/main/java/com/infact/nightour/controller/UsuariosController.java
@@ -1,9 +1,12 @@
 package com.infact.nightour.controller;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
+import android.provider.ContactsContract;
 
 import com.infact.nightour.BancoDeDados;
+import com.infact.nightour.model.Usuario;
 
 /**
  * Created by Timóteo on 22/11/2015.
@@ -14,5 +17,13 @@ public class UsuariosController {
 
     public EventosController(Context context) {
         banco = new BancoDeDados(context);
+    }
+
+    private static ContentValues makeContentValues(Usuario usuario) {
+        ContentValues valores = new ContentValues();
+        valores.put(usuario.BD_NOME, usuario.getNome());
+        valores.put(usuario.BD_STATUS, usuario.getStatus());
+        valores.put(usuario.BD_INTERESSE, usuario.getInteresse());
+        valores.put(usuario.BD_ANIVERSARIO, usuario.getAniversarioUnixTime());
     }
 }
