8674230cc9a5f28ef2bf61ea58a5ac22e65204eb
Timóteo <mechamutoh@gmail.com>
2015-11-21 22:05:51 -0400

    Implementada a classe StringsCampo

--- app/src/main/java/com/infact/nightour/helper/StringsCampo.java
+++ app/src/main/java/com/infact/nightour/helper/StringsCampo.java
@@ -4,5 +4,27 @@ package com.infact.nightour.helper;
  * Created by Timóteo on 21/11/2015.
  */
 public class StringsCampo {
-    
+    private String nomeCampo;
+    private String tipoCampo;
+
+    StringsCampo(String nomeCampo, String tipoCampo) {
+        this.nomeCampo = nomeCampo;
+        this.tipoCampo = tipoCampo;
+    }
+
+    public String getNomeCampo() {
+        return nomeCampo;
+    }
+
+    public void setNomeCampo(String nomeCampo) {
+        this.nomeCampo = nomeCampo;
+    }
+
+    public String getTipoCampo() {
+        return tipoCampo;
+    }
+
+    public void setTipoCampo(String tipoCampo) {
+        this.tipoCampo = tipoCampo;
+    }
 }
