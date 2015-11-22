876a796b65956484c0e9e546f8d4b93eb36fc2ec
Timóteo <mechamutoh@gmail.com>
2015-11-22 02:03:50 -0400

    Adicionada à classe Usuario os dados para uma chave estrangeira

--- app/src/main/java/com/infact/nightour/model/Usuario.java
+++ app/src/main/java/com/infact/nightour/model/Usuario.java
@@ -1,6 +1,7 @@
 package com.infact.nightour.model;
 
 import com.infact.nightour.helper.MakeCreateTableQuery;
+import com.infact.nightour.helper.StringChaveEstrangeira;
 import com.infact.nightour.helper.StringsCampo;
 
 /**
@@ -24,13 +25,17 @@ public class Usuario {
     public static String BD_ANIVERSARIO = "aniversario";
     public static String BD_ANIVERSARIO_TIPO = "integer"; // Usar o Unix time
 
+    public static String BD_IMAGEM_PERFIL = "imagemPerfil"; // Chave estrangeira
+    public static String BD_IMAGEM_REFERENCIA = "PLACEHOLDER"; // Substituir por Foto.NOME_TABELA mais tarde
+
     public static String getCreateTableQuery() {
-        return MakeCreateTableQuery.makeString(BD_TABELA, new StringsCampo[] {
+        return MakeCreateTableQuery.makeString(BD_TABELA, new Object[] {
                 new StringsCampo(BD_ID, BD_ID_TIPO),
                 new StringsCampo(BD_NOME, BD_NOME_TIPO),
                 new StringsCampo(BD_STATUS, BD_STATUS_TIPO),
                 new StringsCampo(BD_INTERESSE, BD_INTERESSE_TIPO),
-                new StringsCampo(BD_ANIVERSARIO, BD_ANIVERSARIO_TIPO)
+                new StringsCampo(BD_ANIVERSARIO, BD_ANIVERSARIO_TIPO),
+                new StringChaveEstrangeira(BD_IMAGEM_PERFIL, BD_IMAGEM_REFERENCIA)
             });
     }
 
