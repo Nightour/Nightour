27bf9c64bd52e7092a3b5bf9210002e841eb5d80
Timóteo <mechamutoh@gmail.com>
2015-11-21 22:09:26 -0400

    Implementado método makeString na classe MakeCreateTableQuery

--- app/src/main/java/com/infact/nightour/helper/MakeCreateTableQuery.java
+++ app/src/main/java/com/infact/nightour/helper/MakeCreateTableQuery.java
@@ -4,5 +4,22 @@ package com.infact.nightour.helper;
  * Created by Timóteo on 21/11/2015.
  */
 public class MakeCreateTableQuery {
-    
+    public static String makeString(String nomeTabela, StringsCampo[] campos) {
+        String query = "CREATE TABLE " + nomeTabela + " (";
+        boolean primeiroCampo = true;
+
+        for (StringsCampo campo: campos) {
+            if (primeiroCampo) {
+                primeiroCampo = false;
+            }
+            else {
+                query += ",";
+            }
+
+            query += campo.getNomeCampo() + " " + campo.getTipoCampo();
+        }
+
+        query += ")";
+        return query;
+    }
 }
