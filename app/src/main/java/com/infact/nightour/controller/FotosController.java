dbc8fdaa1c6a661d8630bf9b83348579cf5f8ed9
Timóteo <mechamutoh@gmail.com>
2015-11-22 02:21:58 -0400

    Implementado FotosController.insereFoto

--- app/src/main/java/com/infact/nightour/controller/FotosController.java
+++ app/src/main/java/com/infact/nightour/controller/FotosController.java
@@ -1,9 +1,11 @@
 package com.infact.nightour.controller;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 
 import com.infact.nightour.BancoDeDados;
+import com.infact.nightour.model.Foto;
 
 /**
  * Created by Timóteo on 22/11/2015.
@@ -14,5 +16,18 @@ public class FotosController {
 
     public FotosController(Context context) {
         banco = new BancoDeDados(context);
+    }
+
+    public long insereFoto(Foto foto) {
+        ContentValues valores = new ContentValues();
+        valores.put(foto.BD_IMAGEM, foto.getImagemBytes());
+        valores.put(foto.BD_DESCRICAO, foto.getDescricao());
+        valores.put(foto.BD_TIMESTAMP, foto.getTimestampUnixTime());
+
+        db = banco.getWritableDatabase();
+        long resultado = db.insert(foto.NOME_TABELA, null, valores);
+        db.close();
+
+        return resultado;
     }
 }
