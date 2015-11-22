f953baeb6bc6e3dfaf19abcc05f32ebb7d4d78d9
Timóteo <mechamutoh@gmail.com>
2015-11-22 01:47:58 -0400

    Criada e implementada a classe StringChaveEstrangeira
    Classe auxiliar que será usada para incluir um campo de chave
    estrangeira na string de uma query para criação de tabela.

---
+++ app/src/main/java/com/infact/nightour/helper/StringChaveEstrangeira.java
@@ -0,0 +1,20 @@
+package com.infact.nightour.helper;
+
+/**
+ * Created by Timóteo on 22/11/2015.
+ */
+public class StringChaveEstrangeira {
+    private String nomeChave;
+
+    public StringChaveEstrangeira(String nomeChave) {
+        this.nomeChave = nomeChave;
+    }
+
+    public String getNomeChave() {
+        return nomeChave;
+    }
+
+    public void setNomeChave(String nomeChave) {
+        this.nomeChave = nomeChave;
+    }
+}
