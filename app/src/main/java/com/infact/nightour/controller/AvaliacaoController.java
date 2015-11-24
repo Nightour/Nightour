676b5e2b8266bb51d80799d92c6b993396e7bcda
Timóteo <mechamutoh@gmail.com>
2015-11-24 19:34:57 -0400

    Implementado AvaliacaoController.makeContentValues()
    Método auxiliar.

--- app/src/main/java/com/infact/nightour/controller/AvaliacaoController.java
+++ app/src/main/java/com/infact/nightour/controller/AvaliacaoController.java
@@ -1,9 +1,11 @@
 package com.infact.nightour.controller;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 
 import com.infact.nightour.BancoDeDados;
+import com.infact.nightour.model.Avaliacao;
 
 /**
  * Created by Timóteo on 24/11/2015.
@@ -14,5 +16,14 @@ public class AvaliacaoController {
 
     public AvaliacaoController(Context context) {
         banco = new BancoDeDados(context);
+    }
+
+    private static ContentValues makeContentValues(Avaliacao avaliacao) {
+        ContentValues valores = new ContentValues();
+        valores.put(avaliacao.BD_QUANT_ESTRELAS, avaliacao.getQuantEstrelas());
+        valores.put(avaliacao.BD_COMENTARIO, avaliacao.getComentario());
+        valores.put(avaliacao.BD_TIMESTAMP, avaliacao.getTimestampUnixTime());
+
+        return valores;
     }
 }
