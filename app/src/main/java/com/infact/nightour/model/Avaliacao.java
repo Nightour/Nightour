f7cb9ba1dc49f18cade650e3a85e4aab0001895c
Timóteo <mechamutoh@gmail.com>
2015-11-24 19:12:54 -0400

    Implementado Avaliacao.getCreateTableQuery()

--- app/src/main/java/com/infact/nightour/model/Avaliacao.java
+++ app/src/main/java/com/infact/nightour/model/Avaliacao.java
@@ -1,5 +1,8 @@
 package com.infact.nightour.model;
 
+import com.infact.nightour.helper.MakeCreateTableQuery;
+import com.infact.nightour.helper.StringsCampo;
+
 /**
  * Created by Timóteo on 24/11/2015.
  */
@@ -17,4 +20,13 @@ public class Avaliacao {
 
     public static String BD_TIMESTAMP = "timestamp";
     public static String BD_TIMESTAMP_TIPO = "int"; // Armazenado em Unix time
+
+    public static String getCreateTableQuery() {
+        return MakeCreateTableQuery.makeString(NOME_TABELA, new StringsCampo[]{
+                new StringsCampo(BD_ID, BD_ID_TIPO),
+                new StringsCampo(BD_QUANT_ESTRELAS, BD_QUANT_ESTRELAS_TIPO),
+                new StringsCampo(BD_COMENTARIO, BD_COMENTARIO_TIPO),
+                new StringsCampo(BD_TIMESTAMP, BD_TIMESTAMP_TIPO)
+        });
+    }
 }
