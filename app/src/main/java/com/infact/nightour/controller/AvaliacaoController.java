299b01ed83bf7c638cde2ed8aa051ecae7cbc301
Timóteo <mechamutoh@gmail.com>
2015-11-24 19:31:18 -0400

    Implementado em AvaliacaoController o construtor

--- app/src/main/java/com/infact/nightour/controller/AvaliacaoController.java
+++ app/src/main/java/com/infact/nightour/controller/AvaliacaoController.java
@@ -1,7 +1,18 @@
 package com.infact.nightour.controller;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.infact.nightour.BancoDeDados;
+
 /**
  * Created by Timóteo on 24/11/2015.
  */
 public class AvaliacaoController {
+    private SQLiteDatabase db;
+    private BancoDeDados banco;
+
+    public AvaliacaoController(Context context) {
+        banco = new BancoDeDados(context);
+    }
 }
