668454bf5b9326842f38377f117452b4b27aed48
Timóteo <mechamutoh@gmail.com>
2015-11-24 19:28:10 -0400

    Adicionadas propriedades à classe Avaliacao

--- app/src/main/java/com/infact/nightour/model/Avaliacao.java
+++ app/src/main/java/com/infact/nightour/model/Avaliacao.java
@@ -3,6 +3,9 @@ package com.infact.nightour.model;
 import com.infact.nightour.helper.MakeCreateTableQuery;
 import com.infact.nightour.helper.StringsCampo;
 
+import java.sql.Time;
+import java.util.Date;
+
 /**
  * Created by Timóteo on 24/11/2015.
  */
@@ -21,6 +24,9 @@ public class Avaliacao {
     public static String BD_TIMESTAMP = "timestamp";
     public static String BD_TIMESTAMP_TIPO = "int"; // Armazenado em Unix time
 
+    public Avaliacao() {
+    }
+
     public static String getCreateTableQuery() {
         return MakeCreateTableQuery.makeString(NOME_TABELA, new StringsCampo[]{
                 new StringsCampo(BD_ID, BD_ID_TIPO),
@@ -28,5 +34,42 @@ public class Avaliacao {
                 new StringsCampo(BD_COMENTARIO, BD_COMENTARIO_TIPO),
                 new StringsCampo(BD_TIMESTAMP, BD_TIMESTAMP_TIPO)
         });
+    }
+
+    private int id;
+    private int quantEstrelas;
+    private String comentario;
+    private Date timestamp;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getQuantEstrelas() {
+        return quantEstrelas;
+    }
+
+    public void setQuantEstrelas(int quantEstrelas) {
+        this.quantEstrelas = quantEstrelas;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
     }
 }
