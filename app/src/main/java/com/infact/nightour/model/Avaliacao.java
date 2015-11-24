8a95a15b0f6b3168549bfc331173e0991b38b3a3
Timóteo <mechamutoh@gmail.com>
2015-11-24 19:10:06 -0400

    Adicionados à classe Avaliacao os campos que guardam dados da tabela

--- app/src/main/java/com/infact/nightour/model/Avaliacao.java
+++ app/src/main/java/com/infact/nightour/model/Avaliacao.java
@@ -4,4 +4,17 @@ package com.infact.nightour.model;
  * Created by Timóteo on 24/11/2015.
  */
 public class Avaliacao {
+    public static String NOME_TABELA = "eventos";
+
+    public static String BD_ID = "_id";
+    public static String BD_ID_TIPO = "integer primary key autoincrement";
+
+    public static String BD_QUANT_ESTRELAS = "quantEstrelas";
+    public static String BD_QUANT_ESTRELAS_TIPO = "integer";
+
+    public static String BD_COMENTARIO = "comentario";
+    public static String BD_COMENTARIO_TIPO = "text";
+
+    public static String BD_TIMESTAMP = "timestamp";
+    public static String BD_TIMESTAMP_TIPO = "int"; // Armazenado em Unix time
 }
