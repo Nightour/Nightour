c071d8c76b093fd32804813c86735d4cbd00516b
Timóteo <mechamutoh@gmail.com>
2015-11-27 16:22:09 -0400

    Adicionados a HomeEventoAdapter construtor e métodos padrão

--- app/src/main/java/com/infact/nightour/adapter/HomeEventoAdapter.java
+++ app/src/main/java/com/infact/nightour/adapter/HomeEventoAdapter.java
@@ -1,7 +1,26 @@
 package com.infact.nightour.adapter;
 
+import android.content.Context;
+import android.database.Cursor;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+
 /**
  * Created by Timóteo on 27/11/2015.
  */
-public class HomeEventoAdapter {
+public class HomeEventoAdapter extends CursorAdapter {
+    public HomeEventoAdapter(Context context, Cursor cursor, int flags) {
+        super(context, cursor, 0);
+    }
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        return null;
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+
+    }
 }
