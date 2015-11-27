c908f0c6d046621a3cd3951a4baa286939b663df
Timóteo <mechamutoh@gmail.com>
2015-11-27 16:44:27 -0400

    Implementado HomeEventoAdapter.bindView()

--- app/src/main/java/com/infact/nightour/adapter/HomeEventoAdapter.java
+++ app/src/main/java/com/infact/nightour/adapter/HomeEventoAdapter.java
@@ -10,6 +10,7 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.example.neto.nightour.R;
+import com.infact.nightour.model.Evento;
 
 /**
  * Created by Timóteo on 27/11/2015.
@@ -26,6 +27,12 @@ public class HomeEventoAdapter extends CursorAdapter {
 
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
+        ImageButton ibItemDescubra = (ImageButton) view.findViewById(R.id.ibIconeEvento_Home);
+        TextView tvItemDescubra = (TextView) view.findViewById(R.id.tvNomeEvento_Home);
 
+        Evento evento = Evento.fromCursor(cursor);
+
+        ibItemDescubra.setImageBitmap(evento.getImagem());
+        tvItemDescubra.setText(evento.getNome());
     }
 }
