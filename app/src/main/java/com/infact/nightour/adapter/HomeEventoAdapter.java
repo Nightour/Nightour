29210ece857a785c3055263be1bad43aa19ca068
Timóteo <mechamutoh@gmail.com>
2015-11-27 16:29:57 -0400

    Implementado HomeEventoAdapter.newView()

--- app/src/main/java/com/infact/nightour/adapter/HomeEventoAdapter.java
+++ app/src/main/java/com/infact/nightour/adapter/HomeEventoAdapter.java
@@ -2,9 +2,14 @@ package com.infact.nightour.adapter;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.example.neto.nightour.R;
 
 /**
  * Created by Timóteo on 27/11/2015.
@@ -16,7 +21,7 @@ public class HomeEventoAdapter extends CursorAdapter {
 
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        return null;
+        return LayoutInflater.from(context).inflate(R.layout.descubra_item, parent, false);
     }
 
     @Override
