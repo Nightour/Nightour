83cd6297f49934d7aeaaf7805fe4e65c63be5852
Timóteo <mechamutoh@gmail.com>
2015-11-26 20:48:26 -0400

    Adicionados à classe Local os dados da tabela

--- app/src/main/java/com/infact/nightour/model/Local.java
+++ app/src/main/java/com/infact/nightour/model/Local.java
@@ -4,4 +4,17 @@ package com.infact.nightour.model;
  * Created by Timóteo on 26/11/2015.
  */
 public class Local {
+    public static String NOME_TABELA = "fotos";
+
+    public static String BD_ID = "_id";
+    public static String BD_ID_TIPO = "integer primary key autoincrement";
+
+    public static final String BD_LATITUDE = "latitude";
+    public static final String BD_LATITUDE_TIPO = "double";
+
+    public static final String BD_LONGITUDE = "longitude";
+    public static final String BD_LONGITUDE_TIPO = "double";
+
+    public static final String BD_ZOOM = "zoom";
+    public static final String BD_ZOOM_TIPO = "text";
 }
