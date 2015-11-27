5b69498cebd72003939629b88957d9b41277fce1
Timóteo <mechamutoh@gmail.com>
2015-11-26 21:02:00 -0400

    Adicionado o construtor à classe LocalController

--- app/src/main/java/com/infact/nightour/controller/LocalController.java
+++ app/src/main/java/com/infact/nightour/controller/LocalController.java
@@ -1,7 +1,18 @@
 package com.infact.nightour.controller;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.infact.nightour.BancoDeDados;
+
 /**
  * Created by Timóteo on 26/11/2015.
  */
 public class LocalController {
+    private SQLiteDatabase db;
+    private BancoDeDados banco;
+
+    public LocalController(Context context) {
+        banco = new BancoDeDados(context);
+    }
 }
