6fed3117b4995cec569f3d7b31e478640c11eb4b
Timóteo <mechamutoh@gmail.com>
2015-11-27 17:14:47 -0400

    Modificada a superclasse de HomeEventoAdapter
    Para poder funcionar com o RecyclerView.

--- app/src/main/java/com/infact/nightour/adapter/HomeEventoAdapter.java
+++ app/src/main/java/com/infact/nightour/adapter/HomeEventoAdapter.java
@@ -2,6 +2,7 @@ package com.infact.nightour.adapter;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,24 +16,26 @@ import com.infact.nightour.model.Evento;
 /**
  * Created by Timóteo on 27/11/2015.
  */
-public class HomeEventoAdapter extends CursorAdapter {
-    public HomeEventoAdapter(Context context, Cursor cursor, int flags) {
-        super(context, cursor, 0);
+public class HomeEventoAdapter extends RecyclerView.Adapter<HomeEventoAdapter.MyViewHolder> {
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
     }
 
     @Override
-    public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        return LayoutInflater.from(context).inflate(R.layout.descubra_item, parent, false);
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+
     }
 
     @Override
-    public void bindView(View view, Context context, Cursor cursor) {
-        ImageButton ibItemDescubra = (ImageButton) view.findViewById(R.id.ibIconeEvento_Home);
-        TextView tvItemDescubra = (TextView) view.findViewById(R.id.tvNomeEvento_Home);
+    public int getItemCount() {
+        return 0;
+    }
 
-        Evento evento = Evento.fromCursor(cursor);
-
-        ibItemDescubra.setImageBitmap(evento.getImagem());
-        tvItemDescubra.setText(evento.getNome());
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        public MyViewHolder(View view) {
+            super(view);
+        }
     }
 }
