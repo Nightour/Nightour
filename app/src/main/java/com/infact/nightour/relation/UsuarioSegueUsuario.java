66d2afa49b6cee49c48f8881aaf9c9837f99aec4
Timóteo <mechamutoh@gmail.com>
2015-12-12 12:02:59 -0400

    Implementado UsuarioSegueUsuario.getCreateTableQuery()

--- app/src/main/java/com/infact/nightour/relation/UsuarioSegueUsuario.java
+++ app/src/main/java/com/infact/nightour/relation/UsuarioSegueUsuario.java
@@ -1,5 +1,7 @@
 package com.infact.nightour.relation;
 
+import com.infact.nightour.helper.MakeCreateTableQuery;
+import com.infact.nightour.helper.StringChaveEstrangeira;
 import com.infact.nightour.model.Usuario;
 
 /**
@@ -17,4 +19,11 @@ public class UsuarioSegueUsuario {
     public static String BD_CHAVE_SEGUIDO_TIPO = "integer";
     public static String BD_SEGUIDO_REFERENCIA = Usuario.NOME_TABELA;
     public static String BD_SEGUIDO_CAMPO_REFERENCIADO = Usuario.BD_ID;
+
+    public static String getCreateTableQuery() {
+        return MakeCreateTableQuery.makeString(NOME_TABELA, new Object[]{
+                new StringChaveEstrangeira(BD_CHAVE_SEGUIDOR, BD_CHAVE_SEGUIDOR_TIPO, BD_SEGUIDOR_REFERENCIA, BD_SEGUIDOR_CAMPO_REFERENCIADO),
+                new StringChaveEstrangeira(BD_CHAVE_SEGUIDO, BD_CHAVE_SEGUIDO_TIPO, BD_SEGUIDO_REFERENCIA, BD_SEGUIDO_CAMPO_REFERENCIADO)
+        });
+    }
 }
