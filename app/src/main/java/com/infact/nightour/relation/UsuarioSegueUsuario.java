d5cae8c9e480691ce1570032700429442f92e1e3
Timóteo <mechamutoh@gmail.com>
2015-12-12 11:53:54 -0400

    UsuarioSegueUsuario: Adicionado o campo estático NOME_TABELA

--- app/src/main/java/com/infact/nightour/relation/UsuarioSegueUsuario.java
+++ app/src/main/java/com/infact/nightour/relation/UsuarioSegueUsuario.java
@@ -4,4 +4,5 @@ package com.infact.nightour.relation;
  * Created by Timóteo on 12/12/2015.
  */
 public class UsuarioSegueUsuario {
+    public static String NOME_TABELA = "usuario_segue_usuario";
 }
