2c6bd56bebd8d46b9b749b3cae9b66867b4f1dd4
Timóteo <mechamutoh@gmail.com>
2016-01-17 16:40:42 -0400

    Implementado UsuarioFoiAEvento.getCreateTableQuery()

--- app/src/main/java/com/infact/nightour/relation/UsuarioFoiAEvento.java
+++ app/src/main/java/com/infact/nightour/relation/UsuarioFoiAEvento.java
@@ -20,4 +20,12 @@ public class UsuarioFoiAEvento {
     public static String BD_CHAVE_EVENTO_TIPO = "INT";
     public static String BD_EVENTO_REFERENCIA = Evento.NOME_TABELA;
     public static String BD_EVENTO_CAMPO_REFERENCIADO = Evento.BD_ID;
+
+    public static String getCreateTableQuery() {
+        return MakeCreateTableQuery.makeString(NOME_TABELA, new Object[]{
+                new StringChaveEstrangeira(BD_CHAVE_USUARIO, BD_CHAVE_USUARIO_TIPO, BD_USUARIO_REFERENCIA, BD_USUARIO_CAMPO_REFERENCIADO),
+                new StringChaveEstrangeira(BD_CHAVE_EVENTO, BD_CHAVE_EVENTO_TIPO, BD_EVENTO_REFERENCIA, BD_EVENTO_CAMPO_REFERENCIADO),
+                "PRIMARY KEY(" + BD_CHAVE_USUARIO + ", " + BD_CHAVE_EVENTO + ")"
+        });
+    }
 }
