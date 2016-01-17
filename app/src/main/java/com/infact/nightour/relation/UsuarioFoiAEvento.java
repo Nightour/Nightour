815a583cf90bfb4a204df34b750d85a6f16e54b9
Timóteo <mechamutoh@gmail.com>
2016-01-17 16:40:22 -0400

    Na classe UsuarioFoiAEvento, adicionados os dados dos campos da tabela

--- app/src/main/java/com/infact/nightour/relation/UsuarioFoiAEvento.java
+++ app/src/main/java/com/infact/nightour/relation/UsuarioFoiAEvento.java
@@ -1,7 +1,23 @@
 package com.infact.nightour.relation;
 
+import com.infact.nightour.helper.MakeCreateTableQuery;
+import com.infact.nightour.helper.StringChaveEstrangeira;
+import com.infact.nightour.model.Evento;
+import com.infact.nightour.model.Usuario;
+
 /**
  * Created by Timóteo on 17/01/2016.
  */
 public class UsuarioFoiAEvento {
+    public static String NOME_TABELA = "usuarios_foram_a_eventos";
+
+    public static String BD_CHAVE_USUARIO = "usuario_chave"; // Chave estrangeira
+    public static String BD_CHAVE_USUARIO_TIPO = "INT";
+    public static String BD_USUARIO_REFERENCIA = Usuario.NOME_TABELA;
+    public static String BD_USUARIO_CAMPO_REFERENCIADO = Usuario.BD_ID;
+
+    public static String BD_CHAVE_EVENTO = "evento_chave"; // Chave estrangeira
+    public static String BD_CHAVE_EVENTO_TIPO = "INT";
+    public static String BD_EVENTO_REFERENCIA = Evento.NOME_TABELA;
+    public static String BD_EVENTO_CAMPO_REFERENCIADO = Evento.BD_ID;
 }
