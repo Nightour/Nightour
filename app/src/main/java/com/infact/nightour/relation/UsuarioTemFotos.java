5875f5e99dfee40ee9f93d351969ebe33061b82c
Timóteo <mechamutoh@gmail.com>
2016-01-17 17:38:13 -0400

    Na classe UsuarioTemFotos, adicionados os dados dos campos da tabela

--- app/src/main/java/com/infact/nightour/relation/UsuarioTemFotos.java
+++ app/src/main/java/com/infact/nightour/relation/UsuarioTemFotos.java
@@ -1,7 +1,21 @@
 package com.infact.nightour.relation;
 
+import com.infact.nightour.model.Foto;
+import com.infact.nightour.model.Usuario;
+
 /**
  * Created by Timóteo on 17/01/2016.
  */
 public class UsuarioTemFotos {
+    public static String NOME_TABELA = "usuarios_foram_a_eventos";
+
+    public static String BD_CHAVE_DONO = "dono_chave"; // Chave estrangeira
+    public static String BD_CHAVE_DONO_TIPO = "INT";
+    public static String BD_DONO_REFERENCIA = Usuario.NOME_TABELA;
+    public static String BD_DONO_CAMPO_REFERENCIADO = Usuario.BD_ID;
+
+    public static String BD_CHAVE_FOTO = "foto_chave"; // Chave estrangeira
+    public static String BD_CHAVE_FOTO_TIPO = "INT";
+    public static String BD_FOTO_REFERENCIA = Foto.NOME_TABELA;
+    public static String BD_FOTO_CAMPO_REFERENCIADO = Foto.BD_ID;
 }
