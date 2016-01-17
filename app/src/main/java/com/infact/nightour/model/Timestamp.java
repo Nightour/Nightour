07e9c618cd484c9217d5af8af499ac2f4df5a609
Timóteo <mechamutoh@gmail.com>
2016-01-17 03:07:10 -0400

    Implementado o método Timestamp.getCreateTableQuery()

--- app/src/main/java/com/infact/nightour/model/Timestamp.java
+++ app/src/main/java/com/infact/nightour/model/Timestamp.java
@@ -1,5 +1,8 @@
 package com.infact.nightour.model;
 
+import com.infact.nightour.helper.MakeCreateTableQuery;
+import com.infact.nightour.helper.StringsCampo;
+
 /**
  * Created by Timóteo on 17/01/2016.
  */
@@ -14,4 +17,12 @@ public class Timestamp {
 
     public static String BD_UPDATE_TIME = "update_time";
     public static String BD_UPDATE_TIME_TIPO = "INT"; // Armazenado em Unix time
+
+    public static String getCreateTableQuery() {
+        return MakeCreateTableQuery.makeString(NOME_TABELA, new StringsCampo[]{
+                new StringsCampo(BD_ID, BD_ID_TIPO),
+                new StringsCampo(BD_CREATE_TIME, BD_CREATE_TIME_TIPO),
+                new StringsCampo(BD_UPDATE_TIME, BD_UPDATE_TIME_TIPO)
+        });
+    }
 }
