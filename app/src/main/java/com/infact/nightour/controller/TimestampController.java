ba215f2cc02c2d0ef538f5cd3aa7868d8a3aa27c
Timóteo <mechamutoh@gmail.com>
2016-01-17 03:18:23 -0400

    Adicionados construtor e campos básicos a TimestampController

--- app/src/main/java/com/infact/nightour/controller/TimestampController.java
+++ app/src/main/java/com/infact/nightour/controller/TimestampController.java
@@ -1,7 +1,18 @@
 package com.infact.nightour.controller;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.infact.nightour.BancoDeDados;
+
 /**
  * Created by Timóteo on 17/01/2016.
  */
 public class TimestampController {
+    private SQLiteDatabase db;
+    private BancoDeDados banco;
+
+    public TimestampController(Context context) {
+        banco = new BancoDeDados(context);
+    }
 }
