7e77e5dde8cd2638f6a80b0cfbe7112ee0d91215
Timóteo <mechamutoh@gmail.com>
2016-01-17 15:51:16 -0400

    Alterado Usuario.BD_IMAGEM_PERFIL
    Para refletir o modelo UML.

--- app/src/main/java/com/infact/nightour/model/Usuario.java
+++ app/src/main/java/com/infact/nightour/model/Usuario.java
@@ -27,7 +27,7 @@ public class Usuario {
     public static String BD_ANIVERSARIO = "aniversario";
     public static String BD_ANIVERSARIO_TIPO = "integer"; // Usar o Unix time
 
-    public static String BD_IMAGEM_PERFIL = "imagemPerfil"; // Chave estrangeira
+    public static String BD_IMAGEM_PERFIL = "fotoPerfil_chave"; // Chave estrangeira
     public static String BD_IMAGEM_PERFIL_TIPO = "integer";
     public static String BD_IMAGEM_REFERENCIA = Foto.NOME_TABELA;
     public static String BD_IMAGEM_CAMPO_REFERENCIADO = Foto.BD_ID;
