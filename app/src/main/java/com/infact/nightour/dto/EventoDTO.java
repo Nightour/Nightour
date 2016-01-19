8c95301779b449eef35dcca8b09799a66021a655
Timóteo <mechamutoh@gmail.com>
2016-01-19 18:40:05 -0400

    Encapsulados os atributos da classe EventoDTO

--- app/src/main/java/com/infact/nightour/dto/EventoDTO.java
+++ app/src/main/java/com/infact/nightour/dto/EventoDTO.java
@@ -11,4 +11,52 @@ public class EventoDTO {
 
     private FotoDTO capa;
     private LocalDTO local;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public FotoDTO getCapa() {
+        return capa;
+    }
+
+    public void setCapa(FotoDTO capa) {
+        this.capa = capa;
+    }
+
+    public LocalDTO getLocal() {
+        return local;
+    }
+
+    public void setLocal(LocalDTO local) {
+        this.local = local;
+    }
 }
