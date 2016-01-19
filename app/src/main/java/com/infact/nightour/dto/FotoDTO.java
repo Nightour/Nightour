7e602ca5d7666474b715b4a0559ee845ffd10c5b
Timóteo <mechamutoh@gmail.com>
2016-01-19 18:42:08 -0400

    Encapsulados os atributos da classe FotoDTO

--- app/src/main/java/com/infact/nightour/dto/FotoDTO.java
+++ app/src/main/java/com/infact/nightour/dto/FotoDTO.java
@@ -8,4 +8,36 @@ public class FotoDTO {
     private byte[] imagem;
     private String descricao;
     private Long timestamp;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public byte[] getImagem() {
+        return imagem;
+    }
+
+    public void setImagem(byte[] imagem) {
+        this.imagem = imagem;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Long timestamp) {
+        this.timestamp = timestamp;
+    }
 }
