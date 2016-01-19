657d76d6f5c1155b324eb7db3c652ff9061fda14
Timóteo <mechamutoh@gmail.com>
2016-01-19 18:33:46 -0400

    Adicionados atributos à classe EventoDTO

--- app/src/main/java/com/infact/nightour/dto/EventoDTO.java
+++ app/src/main/java/com/infact/nightour/dto/EventoDTO.java
@@ -4,4 +4,11 @@ package com.infact.nightour.dto;
  * Created by Timóteo on 19/01/2016.
  */
 public class EventoDTO {
+    private int id;
+    private String nome;
+    private String descricao;
+    private String genero;
+
+    private FotoDTO capa;
+    private LocalDTO local;
 }
