114bd756e78cf2044ff5f03825fac90d135f5772
Timóteo <mechamutoh@gmail.com>
2016-01-19 18:27:07 -0400

    Adicionados atributos à classe FotoDTO

--- app/src/main/java/com/infact/nightour/dto/FotoDTO.java
+++ app/src/main/java/com/infact/nightour/dto/FotoDTO.java
@@ -4,4 +4,8 @@ package com.infact.nightour.dto;
  * Created by Timóteo on 19/01/2016.
  */
 public class FotoDTO {
+    private int id;
+    private byte[] imagem;
+    private String descricao;
+    private Long timestamp;
 }
