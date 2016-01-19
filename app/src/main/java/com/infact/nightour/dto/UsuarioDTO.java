4904873bd379eb3819e74476e699684237df1df3
Timóteo <mechamutoh@gmail.com>
2016-01-19 18:40:23 -0400

    Encapsulados os atributos da classe UsuarioDTO

--- app/src/main/java/com/infact/nightour/dto/UsuarioDTO.java
+++ app/src/main/java/com/infact/nightour/dto/UsuarioDTO.java
@@ -11,4 +11,52 @@ public class UsuarioDTO {
     private String aniversario;
 
     private FotoDTO imagemPerfil;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getInteresse() {
+        return interesse;
+    }
+
+    public void setInteresse(String interesse) {
+        this.interesse = interesse;
+    }
+
+    public String getAniversario() {
+        return aniversario;
+    }
+
+    public void setAniversario(String aniversario) {
+        this.aniversario = aniversario;
+    }
+
+    public FotoDTO getImagemPerfil() {
+        return imagemPerfil;
+    }
+
+    public void setImagemPerfil(FotoDTO imagemPerfil) {
+        this.imagemPerfil = imagemPerfil;
+    }
 }
