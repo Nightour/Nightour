7cd2305a841a2cacc1a6f614cfe8b024ce020b48
Timóteo <mechamutoh@gmail.com>
2016-01-19 18:18:25 -0400

    Adicionados atributos a UsuarioDTO

--- app/src/main/java/com/infact/nightour/dto/UsuarioDTO.java
+++ app/src/main/java/com/infact/nightour/dto/UsuarioDTO.java
@@ -4,4 +4,11 @@ package com.infact.nightour.dto;
  * Created by Timóteo on 19/01/2016.
  */
 public class UsuarioDTO {
+    private int id;
+    private String nome;
+    private String status;
+    private String interesse;
+    private String aniversario;
+
+    private FotoDTO imagemPerfil;
 }
