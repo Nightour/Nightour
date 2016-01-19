b77cee7e02c8318e3d870cad479f899ea3f39d7b
Timóteo <mechamutoh@gmail.com>
2016-01-19 18:33:00 -0400

    Adicionados atributos à classe LocalDTO

--- app/src/main/java/com/infact/nightour/dto/LocalDTO.java
+++ app/src/main/java/com/infact/nightour/dto/LocalDTO.java
@@ -4,4 +4,8 @@ package com.infact.nightour.dto;
  * Created by Timóteo on 19/01/2016.
  */
 public class LocalDTO {
+    private int id;
+    private double longitude;
+    private double latitude;
+    private double zoom;
 }
