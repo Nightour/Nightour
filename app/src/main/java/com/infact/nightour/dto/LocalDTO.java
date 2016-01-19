56d2ebab46176dded08171c6eb2d99d7a84ca8a9
Timóteo <mechamutoh@gmail.com>
2016-01-19 18:40:12 -0400

    Encapsulados os atributos da classe LocalDTO

--- app/src/main/java/com/infact/nightour/dto/LocalDTO.java
+++ app/src/main/java/com/infact/nightour/dto/LocalDTO.java
@@ -8,4 +8,36 @@ public class LocalDTO {
     private double longitude;
     private double latitude;
     private double zoom;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getZoom() {
+        return zoom;
+    }
+
+    public void setZoom(double zoom) {
+        this.zoom = zoom;
+    }
 }
