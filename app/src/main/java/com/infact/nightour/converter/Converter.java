ffeee261e3464b7ead53e13d29a94ce6e3d4597d
Timóteo <mechamutoh@gmail.com>
2016-02-28 17:11:02 -0400

    Adicionado o método DTOtoUsuario à classe Converter
    Converte um UsuarioDTO para uma instância de Usuario.
    
     - Falta fazer com que a FotoDTO do "dto" seja convertida à Foto "imagemPerfil" do "usuario".

--- app/src/main/java/com/infact/nightour/converter/Converter.java
+++ app/src/main/java/com/infact/nightour/converter/Converter.java
@@ -1,7 +1,20 @@
 package com.infact.nightour.converter;
 
+import com.infact.nightour.dto.UsuarioDTO;
+import com.infact.nightour.model.Usuario;
+
 /**
  * Created by Timóteo on 28/02/2016.
  */
 public class Converter {
+    public static Usuario DTOtoUsuario(UsuarioDTO dto) {
+        Usuario usuario = new Usuario();
+
+        usuario.setId(dto.getId());
+        usuario.setStatus(dto.getStatus());
+        usuario.setInteresse(dto.getInteresse());
+        usuario.setAniversario(Long.parseLong(dto.getAniversario(), 10));
+
+        return usuario;
+    }
 }
